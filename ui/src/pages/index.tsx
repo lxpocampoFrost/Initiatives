3b5c09756a4bb7c56fd944a781e4dc1d45e939fc
Marie <marie.garbin@frostdesigngroup.com>
2024-01-22 18:25:52 +0800

    remove component on main page

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,10 +1,9 @@
 import { Box } from '@mui/material';
-import Search from '../components/Search';
 
 export default function Home() {
   return (
     <>
-      <Search />
+      <Box>Hello World!</Box>
     </>
   );
 }
