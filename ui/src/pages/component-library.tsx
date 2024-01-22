6dfafe60041dd9504f808bb1abe05213b1843180
Jm <jm.delasalas@frostdesigngroup.com>
2024-01-22 16:01:40 +0800

    Added post item component

--- ui/src/pages/component-library.tsx
+++ ui/src/pages/component-library.tsx
@@ -1,29 +1,38 @@
 import Box from '@mui/material/Box';
 import Tags from '@/components/Tags';
 import styled from '@emotion/styled';
+import PostItem from '@/components/PostList/PostItem';
 
 const TagWrapper = styled.div`
-    display: flex;
-    gap: 4px
-`
+	display: flex;
+	gap: 4px;
+`;
+
+const postItemData = {
+	title: 'Help with Java Functions',
+	post: "I wanted to take a moment to recognize your outstanding work on the recent [project name] project. Your designs were creative, innovative, and visually stunning. They really helped to bring the project to life. I'm also impressed with your attention to detail and your willingness to go the extra mile. You always met deadlines, even when things got hectic.",
+	created_by: 'John Doe',
+	created_date: 'July 7,2023',
+	tags: ['Html', 'CSS'],
+};
 
 export default function ComponentLibrary() {
 	return (
 		<Box
-            backgroundColor = 'rgba(12, 14, 19, 1)'
-            padding="32px"
-        >
-            <TagWrapper>
-                <Tags 
-                    name='All'
-                />
-                <Tags 
-                    name='Frontend'
-                />
-                <Tags 
-                    name='Backend'
-                />
-            </TagWrapper>
-        </Box>
+			sx={{
+				backgroundColor: 'rgba(12, 14, 19, 1)',
+				padding: '32px',
+				display: 'flex',
+				flexDirection: 'column',
+				gap: '20px',
+			}}
+		>
+			<TagWrapper>
+				<Tags name='All' />
+				<Tags name='Frontend' />
+				<Tags name='Backend' />
+			</TagWrapper>
+			<PostItem data={postItemData} />
+		</Box>
 	);
 }
