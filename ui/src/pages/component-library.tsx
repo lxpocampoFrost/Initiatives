4883d816f0e2e52d5f0974f38ce46ad0233c3525
Marie <marie.garbin@frostdesigngroup.com>
2024-01-22 18:15:09 +0800

    added dropdown component

--- ui/src/pages/component-library.tsx
+++ ui/src/pages/component-library.tsx
@@ -1,29 +1,24 @@
 import Box from '@mui/material/Box';
 import Tags from '@/components/Tags';
 import styled from '@emotion/styled';
+import Dropdown from '@/components/Dropdown/Dropdown';
 
 const TagWrapper = styled.div`
-    display: flex;
-    gap: 4px
-`
+  display: flex;
+  gap: 4px;
+`;
 
 export default function ComponentLibrary() {
-	return (
-		<Box
-            backgroundColor = 'rgba(12, 14, 19, 1)'
-            padding="32px"
-        >
-            <TagWrapper>
-                <Tags 
-                    name='All'
-                />
-                <Tags 
-                    name='Frontend'
-                />
-                <Tags 
-                    name='Backend'
-                />
-            </TagWrapper>
-        </Box>
-	);
+  return (
+    <Box backgroundColor="rgba(12, 14, 19, 1)" padding="32px">
+      <TagWrapper>
+        <Tags name="All" />
+        <Tags name="Frontend" />
+        <Tags name="Backend" />
+      </TagWrapper>
+      <Box marginTop={4}>
+        <Dropdown />
+      </Box>
+    </Box>
+  );
 }
