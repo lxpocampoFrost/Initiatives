e3b4651082f0cbdec06bb0974139bcdbec7b4aa6
Jm <jm.delasalas@frostdesigngroup.com>
2024-01-23 11:38:58 +0800

    Updated custom styles to toolbars

--- ui/src/components/Editor/Editor.tsx
+++ ui/src/components/Editor/Editor.tsx
@@ -1,6 +1,6 @@
 //@ts-nocheck
 import { useEffect, useRef } from 'react';
-
+import { Box } from '@mui/material';
 interface EditorProps {
 	mode?: String;
 	postData?: any;
@@ -24,6 +24,8 @@ const Editor = ({ mode, postData }: EditorProps) => {
 		const ImageTool = (await import('@editorjs/image')).default;
 		const Marker = (await import('@editorjs/marker')).default;
 		const DragDrop = (await import('editorjs-drag-drop')).default;
+		const CodeBox = (await import('@bomdi/codebox')).default;
+		const Table = (await import('@editorjs/table')).default;
 
 		const editor = new EditorJS({
 			holder: 'editor-js',
@@ -65,7 +67,6 @@ const Editor = ({ mode, postData }: EditorProps) => {
 				warning: {
 					class: Warning,
 					inlineToolbar: true,
-					shortcut: 'CMD+SHIFT+W',
 					config: {
 						titlePlaceholder: 'Title',
 						messagePlaceholder: 'Message',
@@ -75,6 +76,7 @@ const Editor = ({ mode, postData }: EditorProps) => {
 					class: ImageTool,
 				},
 				marker: Marker,
+				table: Table,
 			},
 			data:
 				mode === 'view' || mode === 'edit'
@@ -90,6 +92,7 @@ const Editor = ({ mode, postData }: EditorProps) => {
 								},
 							],
 					  },
+			autofocus: true,
 			readOnly: mode === 'view' ? true : false,
 			tunes: ['textVariant'],
 			onChange: () => {
@@ -117,8 +120,100 @@ const Editor = ({ mode, postData }: EditorProps) => {
 
 	return (
 		<>
-			<div id='editor-js'></div>
-			<button onClick={handleSubmit}>Submit Post</button>
+			<Box
+				id='editor-js'
+				sx={{
+					color: '#ffffff',
+					'.ce-popover--opened': {
+						padding: '4px',
+						background: '#16191F',
+						borderRadius: '8px',
+						border: 'none',
+					},
+					'.ce-popover__items': {
+						display: 'flex',
+						flexDirection: 'column',
+						rowGap: '8px',
+						margin: '8px 0',
+						'&::-webkit-scrollbar': {
+							width: '11px',
+							background: '#2C313C',
+						},
+						'&::-webkit-scrollbar-track': {
+							backgroundColor: '#2C313C',
+						},
+						'&::-webkit-scrollbar-thumb': {
+							background: '#4F545F',
+							borderRadius: '8px',
+						},
+					},
+					'.ce-popover-item': {
+						padding: '0',
+						color: '#ffffff',
+					},
+					'.ce-popover-item__title': {
+						fontFamily: 'Figtree-Medium,sans-serif',
+						fontWeight: '500',
+						lineHeight: '16.8px',
+						color: '#ffffff',
+					},
+					'.ce-popover-item__icon': {
+						border: '1px solid #2C313C',
+						borderRadius: '6px',
+						boxShadow: 'none',
+						background: 'transparent',
+						width: '24px',
+						height: '24px',
+					},
+					'.ce-toolbar__plus, .ce-toolbar__settings-btn, .cdx-search-field__icon, .cdx-search-field__icon svg, .ce-popover-item__icon svg': {
+						width: '24px',
+						height: '24px',
+					},
+					'.codex-editor path': {
+						stroke: '#ffffff',
+					},
+					'.cdx-search-field': {
+						background: '#2C313C',
+						borderRadius: '6px',
+						padding: '4px 0px 4px 4px',
+						marginBottom: '0',
+						border: 'none',
+					},
+					'.cdx-search-field__input': {
+						fontFamily: 'Figtree-Medium, sans-serif',
+						fontWeight: '500',
+						fontSize: '14px',
+						lineHeight: '16.8px',
+						color: '#ffffff',
+					},
+					'.cdx-search-field__input::placeholder': {
+						color: '#ffffff',
+						opacity: '0.3',
+					},
+					'.cdx-search-field__icon svg': {
+						color: '#ffffff',
+					},
+					'.ce-popover__custom-content:not(:empty)': {
+						marginBottom: '0',
+					},
+					'.ce-toolbar__settings-btn': {
+						marginLeft: '4px',
+					},
+					'.ce-toolbar__actions': {
+						paddingRight: '8px',
+					},
+					'.ce-block__content': {
+						fontFamily: 'Figtree-Bold,sans-serif',
+					},
+					'.ce-code__textarea': {
+						borderRadius: '8px',
+						background: 'rgba(255, 255, 255, 0.08)',
+						color: '#ffffff',
+						border: 'none',
+					},
+				}}
+			></Box>
+			<button>Submit Post</button>
 		</>
 	);
 };
