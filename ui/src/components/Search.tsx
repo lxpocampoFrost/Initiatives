6fe0029dac295f4abc0607a6bc0a4072852d168e
Marie <marie.garbin@frostdesigngroup.com>
2024-01-23 11:18:32 +0800

    fix click behavior

--- ui/src/components/Search.tsx
+++ ui/src/components/Search.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef } from 'react';
-import { IconButton, TextField } from '@mui/material';
+import React, { useState, useRef, useEffect } from 'react';
+import { Box, IconButton, TextField } from '@mui/material';
 
 const Search = () => {
   const [isTextFieldVisible, setIsTextFieldVisible] = useState(false);
@@ -7,52 +7,66 @@ const Search = () => {
 
   const handleIconClick = () => {
     setIsTextFieldVisible(true);
+  };
 
-    if (inputRef.current) {
+  useEffect(() => {
+    if (isTextFieldVisible && inputRef.current) {
       inputRef.current.focus();
     }
-  };
+  }, [isTextFieldVisible]);
 
   return (
     <>
-      <TextField
-        id="search-bar"
-        className="text"
-        placeholder={isTextFieldVisible ? 'Search' : ''}
-        autoComplete="off"
+      <Box
         sx={{
-          fontFamily: 'Figtree-Regular',
-          fontSize: '16px',
-          fontWeight: '400',
-          lineHeight: '1.5',
-          border: 'none',
-          borderRadius: '0',
-          width: '108px',
-          input: {
-            color: '#fff',
-            height: '24px',
-            padding: '0',
-            backgroundColor: 'transparent',
-            border: 'none',
-            '&::placeholder': {
-              color: '#fff',
-              opacity: '0.2',
-            },
-          },
-          fieldset: {
-            border: 'none',
-          },
+          display: 'flex',
         }}
-      />
-
-      <IconButton
-        onClick={handleIconClick}
-        type="button"
-        aria-label="search"
-        sx={{ width: '24px', height: '24px', marginLeft: '4px' }}
       >
-        <img src="./assets/search-icon.svg" alt="Search Icon" loading="lazy" />
-      </IconButton>
+        <TextField
+          inputRef={inputRef}
+          id="search-bar"
+          className="customText"
+          placeholder="Search"
+          autoComplete="off"
+          sx={{
+            display: isTextFieldVisible ? 'flex' : 'none',
+            fontFamily: 'Figtree-Regular',
+            fontSize: '16px',
+            fontWeight: '400',
+            lineHeight: '1.5',
+            border: 'none',
+            borderRadius: '0',
+            width: '108px',
+            input: {
+              color: '#fff',
+              height: '24px',
+              padding: '0',
+              backgroundColor: 'transparent',
+              border: 'none',
+              '&::placeholder': {
+                color: '#fff',
+                opacity: '0.2',
+              },
+            },
+            fieldset: {
+              border: 'none',
+            },
+          }}
+        />
+
+        <IconButton
+          onClick={handleIconClick}
+          type="button"
+          aria-label="search"
+          sx={{ width: '24px', height: '24px', marginLeft: '4px' }}
+        >
+          <img
+            src="./assets/search-icon.svg"
+            alt="Search Icon"
+            loading="lazy"
+          />
+        </IconButton>
+      </Box>
     </>
   );
 };
